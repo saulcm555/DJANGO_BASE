8021afed9f73c4eed0b2403caf9b6e1f3f64207e
lesquel <holahi23holahi23@gmail.com>
2024-12-21 15:40:25 -0500

    layout

--- tinocoLoco/settings.py
+++ tinocoLoco/settings.py
@@ -153,6 +153,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Asumiendo que tienes una carpeta "static" en tu directorio base
+]
+
+# Directorio donde se almacenarán los archivos estáticos recopilados por 'collectstatic'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- tinocoLoco/urls.py
+++ tinocoLoco/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
     path('servicios/', include('servicios.urls')),
