b66d442a232ea3d440b8f98af2b21afa68c695b8
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-12-21 15:36:50 -0500

    .

--- core/contex_processors.py
+++ core/contex_processors.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 from negocio.models import ConfiguracionNegocio
 
-def settings_context(request):
+def configuracion_negocio_context(request):
     return {'configuracion': ConfiguracionNegocio.objects.first()}
