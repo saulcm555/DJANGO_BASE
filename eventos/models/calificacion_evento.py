bc051f8c9a6bbc43179e0b19754ffc33788d7f4e
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-12-21 17:28:17 -0500

    .

--- eventos/models/calificacion_evento.py
+++ eventos/models/calificacion_evento.py
@@ -9,18 +9,27 @@ class CalificacionEvento(models.Model):
     class Meta:
         verbose_name = "Calificación de Evento"
         verbose_name_plural = "Calificaciones de Eventos"
+
     evento = models.ForeignKey(
-        Evento, on_delete=models.CASCADE, related_name="calificacion_evento", verbose_name="Evento"
+        Evento,
+        on_delete=models.CASCADE,
+        related_name="calificacion_evento",
+        verbose_name="Evento",
     )
     usuario = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="calificacion_evento", verbose_name="Usuario"
+        User,
+        on_delete=models.CASCADE,
+        related_name="calificacion_evento",
+        verbose_name="Usuario",
     )
     calificacion = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)], verbose_name="Calificación"
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        verbose_name="Calificación",
     )
     comentario = models.TextField(verbose_name="Comentario")
-    fecha_publicacion = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Publicación")
+    fecha_publicacion = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de Publicación"
+    )
 
     def __str__(self):
         return f"{self.usuario} - {self.evento} - {self.fecha_publicacion}"
-
